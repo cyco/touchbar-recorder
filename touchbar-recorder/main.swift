20a7520c714024125673f3d9aba31e826f3d6995
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2018-02-19 21:51:42 +0100

    Initialize Xcode project.

---
+++ touchbar-recorder/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  touchbar-recorder
+//
+//  Created by Christoph Leimbrock on 19.02.18.
+//  Copyright © 2018 Christoph Leimbrock. All rights reserved.
+//
+
+import Foundation
+
