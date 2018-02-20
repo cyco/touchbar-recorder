38a644c3576004efd8570fcbb95765be6d099372
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2018-02-20 22:58:26 +0100

    Lookup user directory at runtime.

--- touchbar-recorder/main.swift
+++ touchbar-recorder/main.swift
@@ -8,7 +8,8 @@
 
 import Foundation
 
-let outputPath = "/Users/chris/Movies/Touchbar Recording.mp4"
+
+let output = URL(fileURLWithPath: "Movies/TouchBar Recording.mp4", isDirectory: false, relativeTo: FileManager.default.homeDirectoryForCurrentUser)
 let recorder = TouchbarRecorder()
 
 recorder.start()
@@ -16,4 +17,4 @@ while recorder.isRecording {
     RunLoop.main.run(mode: .defaultRunLoopMode, before: Date(timeIntervalSinceNow: 0.01))
 }
 
-recorder.write(to: URL(fileURLWithPath: outputPath))
+recorder.write(to: output)
