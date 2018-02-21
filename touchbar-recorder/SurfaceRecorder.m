9943949b8b1a2e4489c85f18d918e350a6c66632
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2018-02-21 20:30:47 +0100

    Move IOSurface recording into separate class.

---
+++ touchbar-recorder/SurfaceRecorder.m
@@ -0,0 +1,80 @@
+//
+//  SurfaceRecorder.m
+//  touchbar-recorder
+//
+//  Created by Christoph Leimbrock on 21.02.18.
+//  Copyright © 2018 Christoph Leimbrock. All rights reserved.
+//
+
+#import "SurfaceRecorder.h"
+#import <QuartzCore/QuartzCore.h>
+#import <AVFoundation/AVFoundation.h>
+
+@interface SurfaceRecorder ()
+@property (readwrite) NSURL *outputFile;
+@property (readwrite) AVAssetWriter *writer;
+@property AVAssetWriterInput *videoInput;
+@property AVAssetWriterInputPixelBufferAdaptor *adaptor;
+@property uint64_t startTime;
+@end
+
+@implementation SurfaceRecorder
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        self.outputFile = [NSURL fileURLWithPathComponents:@[NSTemporaryDirectory(), [NSUUID new].UUIDString]];
+    }
+    return self;
+}
+
+- (void)setupForSize:(CGSize)size {
+    self.writer = [AVAssetWriter assetWriterWithURL:self.outputFile fileType:AVFileTypeMPEG4 error:nil];
+    self.videoInput = [AVAssetWriterInput assetWriterInputWithMediaType:AVMediaTypeVideo outputSettings:
+                       @{
+                         AVVideoCodecKey: AVVideoCodecTypeH264,
+                         AVVideoWidthKey: @(size.width * 2),
+                         AVVideoHeightKey: @(size.height  * 2),
+                         AVVideoCompressionPropertiesKey: @{
+                                 AVVideoMaxKeyFrameIntervalKey: @(60)
+                                 }
+                         }];
+    self.videoInput.expectsMediaDataInRealTime = YES;
+    self.adaptor = [AVAssetWriterInputPixelBufferAdaptor assetWriterInputPixelBufferAdaptorWithAssetWriterInput:self.videoInput sourcePixelBufferAttributes:@{}];
+    [self.writer addInput:self.videoInput];
+}
+
+- (void)recordSurface:(IOSurfaceRef)frameSurface at:(uint64_t)displayTime {
+    if (self.writer.status == AVAssetWriterStatusUnknown) {
+        [self.writer startWriting];
+        [self.writer startSessionAtSourceTime:kCMTimeZero];
+        self.startTime = displayTime;
+    }
+    
+    
+    if(self.writer.status == AVAssetWriterStatusFailed) {
+        return;
+    }
+    
+    CVPixelBufferRef pixelBuffer;
+    CVReturn ret;
+    NSDictionary *pixelBufferAttributes = @{
+                                            (NSString*)kCVPixelBufferPixelFormatTypeKey : @(kCVPixelFormatType_32BGRA)
+                                            };
+    
+    ret = CVPixelBufferCreateWithIOSurface(nil, frameSurface, (__bridge CFDictionaryRef _Nullable)(pixelBufferAttributes), &pixelBuffer);
+    CVPixelBufferRetain(pixelBuffer);
+    if(ret != kCVReturnSuccess) {
+        return;
+    }
+    
+    while(!_adaptor.assetWriterInput.isReadyForMoreMediaData) {
+        usleep(100);
+    }
+    
+    [self.adaptor appendPixelBuffer:pixelBuffer withPresentationTime:CMTimeMake(displayTime - self.startTime, NSEC_PER_SEC)];
+    CVPixelBufferRelease(pixelBuffer);
+}
+
+- (void)cleanup {}
+@end
--- touchbar-recorder/TouchbarRecorder.m
+++ touchbar-recorder/TouchbarRecorder.m
@@ -10,14 +10,12 @@
 #import <QuartzCore/QuartzCore.h>
 #import <AVFoundation/AVFoundation.h>
 #import "DFR.h"
+#import "SurfaceRecorder.h"
 
 @interface TouchbarRecorder () <NSMachPortDelegate>
-@property NSURL *outputFile;
-@property AVAssetWriter *writer;
-@property AVAssetWriterInput *videoInput;
-@property AVAssetWriterInputPixelBufferAdaptor *adaptor;
 @property CGDisplayStreamRef stream;
 @property dispatch_queue_t queue;
+@property SurfaceRecorder *recorder;
 @end
 
 @implementation TouchbarRecorder
@@ -25,7 +23,6 @@
 - (instancetype)init
 {
     if (self = [super init]) {
-        self.outputFile = [NSURL fileURLWithPathComponents:@[NSTemporaryDirectory(), [NSUUID new].UUIDString]];
     }
     
     return self;
@@ -36,8 +33,10 @@
     
     self.queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0) ;
     dispatch_async(self.queue, ^{
-        [self setupAssetWriter];
         [self createDisplayStream];
+    
+        self.recorder = [SurfaceRecorder new];
+        [self.recorder setupForSize:[DFR GetScreenSize]];
         
         if(self.stream) {
             CGDisplayStreamStart(self.stream);
@@ -45,40 +44,11 @@
     });
 }
 
-- (void)setupAssetWriter {
-    CGSize size = [DFR GetScreenSize];
-    self.writer = [AVAssetWriter assetWriterWithURL:self.outputFile fileType:AVFileTypeMPEG4 error:nil];
-    self.videoInput = [AVAssetWriterInput assetWriterInputWithMediaType:AVMediaTypeVideo outputSettings:
-                       @{
-                         AVVideoCodecKey: AVVideoCodecTypeH264,
-                         AVVideoWidthKey: @(size.width * 2),
-                         AVVideoHeightKey: @(size.height  * 2),
-                         AVVideoCompressionPropertiesKey: @{
-                                 AVVideoMaxKeyFrameIntervalKey: @(60)
-                                 }
-                         }];
-    self.videoInput.expectsMediaDataInRealTime = YES;
-    self.adaptor = [AVAssetWriterInputPixelBufferAdaptor assetWriterInputPixelBufferAdaptorWithAssetWriterInput:self.videoInput sourcePixelBufferAttributes:@{}];
-    [self.writer addInput:self.videoInput];
-}
 
 - (void)createDisplayStream {
-    __block uint64_t start;
-    self.stream = [DFR DisplayStreamCreate:0 queue:self.queue handler:^(CGDisplayStreamFrameStatus status, uint64_t displayTime, IOSurfaceRef  _Nullable frameSurface, CGDisplayStreamUpdateRef  _Nullable updateRef) {
+    self.stream = [DFR DisplayStreamCreate:0 queue:self.queue handler:^(CGDisplayStreamFrameStatus status, uint64_t displayTime, IOSurfaceRef  _Nullable surface, CGDisplayStreamUpdateRef  _Nullable updateRef) {
         if(!self.isRecording) return;
-        
-        if (self.writer.status == AVAssetWriterStatusUnknown) {
-            [self.writer startWriting];
-            [self.writer startSessionAtSourceTime:kCMTimeZero];
-            start = displayTime;
-        }
-        
-        
-        if(self.writer.status == AVAssetWriterStatusFailed) {
-            return;
-        }
-        
-        if(!frameSurface) {
+        if(!surface) {
             return;
         }
         
@@ -86,24 +56,8 @@
             return;
         }
         
-        CVPixelBufferRef pixelBuffer;
-        CVReturn ret;
-        NSDictionary *pixelBufferAttributes = @{
-                                                (NSString*)kCVPixelBufferPixelFormatTypeKey : @(kCVPixelFormatType_32BGRA)
-                                                };
-        
-        ret = CVPixelBufferCreateWithIOSurface(nil, frameSurface, (__bridge CFDictionaryRef _Nullable)(pixelBufferAttributes), &pixelBuffer);
-        CVPixelBufferRetain(pixelBuffer);
-        if(ret != kCVReturnSuccess) {
-            return;
-        }
-        
-        while(!_adaptor.assetWriterInput.isReadyForMoreMediaData) {
-            usleep(100);
-        }
-        
-        [self.adaptor appendPixelBuffer:pixelBuffer withPresentationTime:CMTimeMake(displayTime - start, NSEC_PER_SEC)];
-        CVPixelBufferRelease(pixelBuffer);
+
+        [self.recorder recordSurface:surface at:displayTime];
     }];
 }
 
@@ -118,7 +72,7 @@
         dispatch_block_t block = ^{
             dispatch_semaphore_signal(sem);
         };
-        [self.writer finishWritingWithCompletionHandler:block];
+        [self.recorder.writer finishWritingWithCompletionHandler:block];
         
         _isRecording = NO;
         while(dispatch_semaphore_wait(sem, dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)))) ;
@@ -126,17 +80,7 @@
 }
 
 - (void)writeTo:(NSURL*)url {
-    [[NSFileManager defaultManager] removeItemAtURL:url error:nil];
-    if([[NSFileManager defaultManager] moveItemAtURL:self.outputFile toURL:url error:nil]) {
-        self.outputFile = nil;
-    }
-    
-    [self cleanup];
-}
-
-- (void)cleanup {
-    if(self.outputFile) {
-        [[NSFileManager defaultManager] removeItemAtURL:self.outputFile error:nil];
-    }
+    [[NSFileManager defaultManager] moveItemAtURL:self.recorder.outputFile toURL:url error:nil];
+    [self.recorder cleanup];
 }
 @end
